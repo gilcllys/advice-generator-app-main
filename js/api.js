294f9277d1e75ce55bac72ba6b89308eb54cf762
gilcllys <gilcllyscosta@gmail.com>
2022-03-17 17:08:06 -0400

    branch de designn iniciada

--- js/api.js
+++ js/api.js
@@ -13,7 +13,6 @@ function ConsumeApi(URL){
         .then(dado =>{
             let id = String(dado.slip.id)
             let advice = String(dado.slip.advice)
-            console.log(advice)
             CreateEmelent(id,advice)   
         })
         
@@ -24,23 +23,20 @@ function CreateEmelent(id,advice){
         // creating a tag h2
         const code = document.createElement('h2')
         //adding the text to the tag h2
-        code.innerHTML= String(id)
+        code.innerHTML= "ADVICE" + "#" +String(id)
 
         // creating a tag h1
         const quote = document.createElement('h1')
         //adding the text to the tag h1
-        quote.innerHTML= String(advice)
+        quote.innerHTML= "\"" + String(advice) + "\""
 
         //appending the elements that were created
         container.appendChild(code)
         container.appendChild(quote)
 }
 ConsumeApi(URL_api)
-
 //add a event to reload all the page
 btn.addEventListener('click',()=> document.location.reload(true))
-
-
 
 
 
